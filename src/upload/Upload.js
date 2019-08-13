7e84c77ede91256a4f1e91eb476b237925f63f9c
Guilherme Estevão <guilhermeestevao@Insights-Mac-mini.local>
2019-08-13 12:22:46 -0300

    Tentando plotar o grafo

--- src/upload/Upload.js
+++ src/upload/Upload.js
@@ -1,46 +1,43 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import { Upload, Icon } from 'antd';
 const { Dragger } = Upload;
 import 'antd/dist/antd.css';
+import Graoh from 'react-graph-vis';
 
-const uploadHandler = file => {
+class UploadGraph extends PureComponent {
 
-}
+  state = {
+    graphProps: null
+  }
 
-
-const upload = () =>{
+  render() {
 
     const props = {
-        name: 'file',
-        multiple: false,
-        action: uploadHandler,
-        customRequest: ({ file, 
-          onSuccess }) => {
-          var reader = new FileReader();
-      
-          reader.onload = function(e) {
-            console.log(e.target.result);
-            onSuccess("ok");
-          };
-      
-          reader.readAsText(file);
-        },
-        onChange(info) {
-          const { file } = info;
-          const status = file.status;
-          if (status !== 'uploading') {
-            console.log(info.file, info.fileList);
-          }
-          if (status === 'done') {
-            console.log(`${info.file.name} file uploaded successfully.`);
-          } else if (status === 'error') {
-            console.log(`${info.file.name} file upload failed.`);
-          }
+      name: 'file',
+      multiple: false,
+      action: "memory",
+      customRequest: this.uploadHandler,
+      onChange(info) {
+        const { file } = info;
+        const status = file.status;
+        if (status !== 'uploading') {
+          console.log(info.file, info.fileList);
+        }
+        if (status === 'done') {
+          console.log(`${info.file.name} file uploaded successfully.`);
+        } else if (status === 'error') {
+          console.log(`${info.file.name} file upload failed.`);
+        }
+      }
+    };
 
-        },
-      };
+
+    const { graphProps } = this.state;
+
+    
 
     return (
+      <React.Fragment>
         <Dragger {...props}>
             <p className="ant-upload-drag-icon">
                 <Icon type="inbox" />
@@ -51,8 +48,81 @@ const upload = () =>{
                 band files
         </p>
         </Dragger>
+        {graphProps && <Graoh {...graphProps} />}
+      </React.Fragment>
     );
+  }
+
+  uploadHandler = ({ file, onSuccess }) => {
+    var reader = new FileReader();
+    reader.onload = (e) => {
+      const lines = e.target.result.split("\n") || [];
+
+      const edges = lines.map(line => {
+        const tokens = line.split(" ");
+        return {
+          from: tokens[0],
+          to: tokens[1]
+        }
+      });
+
+      let nodesIntegers = [];
+
+      for(const line in lines){
+        const tokens = line.split(" ");
+        
+        const node1 = parseInt(tokens[0]);
+        const node2 = parseInt(tokens[1]);
+
+        if(!nodesIntegers.includes(node1))
+          nodesIntegers.push(node1);
+        if(!nodesIntegers.includes(node2))
+          nodesIntegers.push(node2);
+      }
+
+      console.log(nodesIntegers);
+
+      const nodes = nodesIntegers.map( node  =>  {
+        return {
+          id: node,
+          label: `Node ${node}`
+        }
+      });
+
+
+      const options = {
+        layout: {
+            hierarchical: true
+        },
+        edges: {
+            color: "#000000"
+        }
+      };
+
+      
+      console.log("Nodes "+nodes.length)
+      console.log("Edges "+edges.length)
+     
+      this.setGraph({
+        graph: {
+          nodes, edges
+        }, 
+        options
+      });
+
+      onSuccess("ok");
+    };
+
+    reader.readAsText(file);
+  }
+
+  setGraph = graphProps => {
+    this.setState({
+      graphProps
+    })
+  }
+  
 
 }
 
-export default upload;
+export default UploadGraph;
